a61e69c92be756ef0e18afa9d718c61d8d511c27
Nosthertus <nosthertus@gmail.com>
2016-08-27 04:13:51 -0400

    ADDED: CORS on verbs
    *Added CORS settings to all verbs

--- server/index.php
+++ server/index.php
@@ -1,12 +1,35 @@
 <?php
 require "vendor/autoload.php";
 
-$app = new \Slim\App;
+$app = new \Slim\App([
+	"settings" => [
+		"determineRouteBeforeAppMiddleware" => true
+	]
+]);
 
-$app->get("/", function($request, $response){
-	$response = $response->getBody()->write("Hello World");
+$app->add(function($request, $response, $next)
+{
+	$route = $request->getAttribute("route");
 
-	return $response;
+	$methods = [];
+
+	if(count($route->getArguments()) > 0)
+	{
+		$methods = ["GET", "PUT", "DELETE", "OPTIONS"];
+	}
+
+	else
+	{
+		$methods = ["GET, POST", "OPTIONS"];
+	}
+
+	// Set CORS headers
+	$response = $response->withHeader("Access-Control-Allow-Origin", "*");
+	$response = $response->withHeader("Access-Control-Allow-Methods", implode(", ", $methods));
+	$response = $response->withHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow content-type other than defaults
+	$response = $response->withHeader("Access-Control-Max-Age", "86400"); // 24hrs for preflight cache
+
+	return $next($request, $response);
 });
 
 include "routes/accounts.php";
