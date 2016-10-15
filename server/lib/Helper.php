1d2a9912a10e690fb66c73ae0615c3b278264096
Nosthertus <nosthertus@gmail.com>
2016-10-14 20:06:25 -0400

    ADDED: ip resolve function in helper class
    *Added function to gather ip from the request

--- server/lib/Helper.php
+++ server/lib/Helper.php
@@ -76,4 +76,51 @@ Class Helper
 
 		return $var;
 	}
+
+	/**
+	 * Returns the ip from the client
+	 * 
+	 * @return String The ip value found from the client
+	 */
+	public static function get_ip()
+	{
+		$ip_address = "";
+
+		if(getenv("HTTP_CLIENT_IP"))
+		{
+			$ip_address = getenv("HTTP_CLIENT_IP");
+		}
+
+		else if (getenv("HTTP_X_FORWARDED_FOR"))
+		{
+			$ip_address = getenv("HTTP_X_FORWARDED_FOR");
+		}
+
+		else if (getenv("HTTP_X_FORWARDED"))
+		{
+			$ip_address = getenv("HTTP_X_FORWARDED");
+		}
+
+		else if (getenv("HTTP_FORWARDED_FOR"))
+		{
+			$ip_address = getenv("HTTP_FORWARDED_FOR");
+		}
+
+		else if (getenv("HTTP_FORWARDED"))
+		{
+			$ip_address = getenv("HTTP_FORWARDED");
+		}
+
+		else if (getenv("REMOTE_ADDR"))
+		{
+			$ip_address = getenv("REMOTE_ADDR");
+		}
+
+		else
+		{
+			$ip_address = "UNKNOWN";
+		}
+
+		return $ip_address;
+	}
 }
