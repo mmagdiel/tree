2da619cd535dfeebf7e908d870e8e4596617d183
magdielikari <magdielikari@gmail.com>
2016-11-05 00:27:35 -0400

    Frontend mejorado...

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5941,6 +5941,90 @@
 
   'use strict';
 
+    // Pass the documentsIndexCtrl to the app
+    angular
+        .module('y')
+        .controller('documentsIndexCtrl', documentsIndexCtrl);
+
+
+    // Define the documentsIndexCtrl
+    function documentsIndexCtrl(documentsFactory, $state) {
+
+
+        // Inject with ng-annotate
+        "ngInject";
+
+
+        // Define documentsIndex as this for ControllerAs and auto-$scope
+        var documentsIndex = this;
+
+
+        // Define the documentsIndex functions and objects that will be passed to the view
+        documentsIndex.documents = [];                                              // Array for list of documents
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Contrsucts function
+        |--------------------------------------------------------------------------
+        |
+        | All functions that should be init when the controller start
+        |
+        */
+
+
+        initLog();
+        index();
+
+        /*
+        |--------------------------------------------------------------------------
+        | Functions
+        |--------------------------------------------------------------------------
+        |
+        | Declaring all functions used in the documentsIndexCtrl
+        |
+        */
+
+        documentsIndex.go = function(state,id){
+            $state.go(state,{
+                id: id
+            });
+        }
+
+        // Sample for init function
+        function initLog() {
+
+            console.log('documentsIndexCtrl init');
+        }
+
+
+        // Get all documents.
+        function index() {
+
+            return documentsFactory.index().then(function(data) {
+
+                // Custom function for success handling
+                console.log('Result form API with SUCCESS', data);
+
+            	// Assign data to array and return them
+	            documentsIndex.documents = data.data;
+	            return documentsIndex.documents;
+
+            }, function(data) {
+
+                // Custom function for error handling
+                console.log('Result form API with ERROR', data);
+
+            });
+        }
+    }
+
+})();
+
+(function() {
+
+  'use strict';
+
     // Pass the documentsShowCtrl to the app
     angular
         .module('y')
@@ -6104,90 +6188,6 @@
 			});
 		}
 	}
-
-})();
-
-(function() {
-
-  'use strict';
-
-    // Pass the documentsIndexCtrl to the app
-    angular
-        .module('y')
-        .controller('documentsIndexCtrl', documentsIndexCtrl);
-
-
-    // Define the documentsIndexCtrl
-    function documentsIndexCtrl(documentsFactory, $state) {
-
-
-        // Inject with ng-annotate
-        "ngInject";
-
-
-        // Define documentsIndex as this for ControllerAs and auto-$scope
-        var documentsIndex = this;
-
-
-        // Define the documentsIndex functions and objects that will be passed to the view
-        documentsIndex.documents = [];                                              // Array for list of documents
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Contrsucts function
-        |--------------------------------------------------------------------------
-        |
-        | All functions that should be init when the controller start
-        |
-        */
-
-
-        initLog();
-        index();
-
-        /*
-        |--------------------------------------------------------------------------
-        | Functions
-        |--------------------------------------------------------------------------
-        |
-        | Declaring all functions used in the documentsIndexCtrl
-        |
-        */
-
-        documentsIndex.go = function(state,id){
-            $state.go(state,{
-                id: id
-            });
-        }
-
-        // Sample for init function
-        function initLog() {
-
-            console.log('documentsIndexCtrl init');
-        }
-
-
-        // Get all documents.
-        function index() {
-
-            return documentsFactory.index().then(function(data) {
-
-                // Custom function for success handling
-                console.log('Result form API with SUCCESS', data);
-
-            	// Assign data to array and return them
-	            documentsIndex.documents = data.data;
-	            return documentsIndex.documents;
-
-            }, function(data) {
-
-                // Custom function for error handling
-                console.log('Result form API with ERROR', data);
-
-            });
-        }
-    }
 
 })();
 
